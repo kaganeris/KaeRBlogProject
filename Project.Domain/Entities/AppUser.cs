5664ab7656e68392ef1d7150c4bed106265cfb66
Kağan Eriş <123362258+kaganeris@users.noreply.github.com>
2023-12-06 22:42:46 +0300

    Add project files.

---
+++ Project.Domain/Entities/AppUser.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Project.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Domain.Entities
+{
+    public class AppUser : IdentityUser<Guid>, IBaseEntitiy
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        [NotMapped]
+        public string FullName => FirstName + LastName;
+        public DateTime BirthDate { get; set; }
+        public string ImagePath { get; set; }
+        public int ConfirmCode { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+        public DateTime? DeletedDate { get; set; }
+        public Status Status { get; set; }
+    }
+}
---
+++ Project.Domain/Entities/IBaseEntitiy.cs
@@ -0,0 +1,17 @@
+﻿using Project.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Domain.Entities
+{
+    public interface IBaseEntitiy
+    {
+        public DateTime CreatedDate { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+        public DateTime? DeletedDate { get; set; }
+        public Status Status { get; set; }
+    }
+}
